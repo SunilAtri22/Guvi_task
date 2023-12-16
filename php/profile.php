86a1f62877642113b9361bbfd4c2259b143fb969
SunilAtri22 <sunilatri22@gmail.com>
2023-12-16 21:31:19 +0530

    first commit

---
+++ php/profile.php
@@ -0,0 +1,32 @@
+<?php 
+session_start();
+$name=$_POST['name'];
+$age=$_POST['age'];
+$contactnumber=$_POST['contactnumber'];
+$dob=$_POST['dob'];
+$email = $_SESSION['email'];
+$conn=new mysqli('localhost','root','','db_profile');
+
+if($conn->connect_error)
+{
+    die('Connection Failed: '.$conn->connect_error);
+}
+else{
+    
+    
+    $stmt = $conn->prepare("UPDATE details SET name=?, age=?, contactnumber=?, dob=? WHERE email=?");
+    $stmt->bind_param("siiss", $name, $age, $contactnumber, $dob, $email);
+
+    $stmt->execute();
+    echo "<script>
+             
+                alert('Profile Updated');
+                window.location.href = '../profile.html';
+            
+          </script>";
+        exit();
+    $stmt->close();
+    $conn->close();
+}
+
+?>
